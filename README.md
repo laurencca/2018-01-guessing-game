f25635de313b391f9c426d6a88d74b17bc6899c6
Lauren Carroll-Allan <lcarrollallan@gmail.com>
2018-02-06 20:10:09 -0800

    finished basic assignment and updated readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 2018-01-guessing-game
 
-This project is in response to the assignment for Class 3
+This project is in response to the assignment for Class 3:
 [**Class 3 Assignment**](https://github.com/acl-nw-bootcamp/2018-winter/tree/master/week_1-2/class-03)
 
 # Step 1
@@ -13,5 +13,8 @@ The second step is updating this here README folder using some of the tags we le
 ## Here is a smaller heading
 * Here is a bullet point
 - Here is another bullet point
-*Here is some italic text
-**Here is some bold text
+* *Here is some italic text*
+* **Here is some bold text**
+
+# Step 3
+For the Class 4 assignment I created another new branch, and added more functionality to the JS code, including writing the answers to the page using the getElementById method. I also figured out how to fix the formatting issues from my first README draft by adding additional asterisks after the *italic* and **bold** texts to complete the formatting. 
--- main.js
+++ main.js
@@ -6,57 +6,65 @@ var counter = 0;
 
 function birthplace(){
   var answer = prompt("Did I grow up in Oregon?");
+  var message = "";
   console.log("Birthplace: " + answer);
   if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
-    alert("You're right, " + userName + " - I grew up in Oregon!");
+    message = ". You're right, " + userName + " - I grew up in Oregon!";
     counter++;
   } else {
-    alert("You're wrong, " + userName + " - I did grow up in Oregon.");
+    message = ". You're wrong, " + userName + " - I did grow up in Oregon.";
   }
+  document.getElementById("question1").innerHTML = answer + message;
 }
 birthplace();
 
 function college(){
   var answer = prompt("Did I go to Reed College?");
+  var message = "";
   console.log("College: " + answer);
   if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
-    alert("You're wrong, " + userName + " - I didn't go to Reed College. I went to Dartmouth College.");
+    message = ". You're wrong, " + userName + " - I didn't go to Reed College. I went to Dartmouth College.";
   } else {
-    alert("You're right, " + userName + " - I didn't go to Reed College. I went to Dartmouth College!");
+    message = ". You're right, " + userName + " - I didn't go to Reed College. I went to Dartmouth College!";
     counter++;
   }
+  document.getElementById("question2").innerHTML = answer + message;
 }
 college();
 
 function currentHome(){
   var answer = prompt("Do I live in Portland?");
+  var message = "";
   console.log("Current Home: " + answer);
   if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
-    alert("You're wrong, " + userName + " - I don't live in Portland. I live in Hillsboro.");
+    message = ". You're wrong, " + userName + " - I don't live in Portland. I live in Hillsboro.";
   } else {
-    alert("You're right, " + userName + " - I don't live in Portland. I live in Hillsboro!");
+    message = ". You're right, " + userName + " - I don't live in Portland. I live in Hillsboro!";
     counter++;
   }
+  document.getElementById("question3").innerHTML = answer + message;
 }
 currentHome();
 
 function numberGuess(){
   var answer = 0;
+  var message = "";
   console.log("Number " + answer);
   while (answer != 10){
     answer = Number(prompt("Pick a number between 1 - 20"));
     if (answer < 10) {
-      alert("Sorry, " + userName + " - That number is too low.");
+      message = ". Sorry, " + userName + " - That number is too low.";
     } else if (answer > 10) {
-      alert("Sorry, " + userName + " - That number is too high.");
+      message = ". Sorry, " + userName + " - That number is too high.";
     } else if (answer === 10) {
-      alert("Great job, " + userName + " - that's the right number!");
+      message = ". Great job, " + userName + " - that's the right number!";
       counter++;
     } else {
-      alert("That's not a valid entry, " + userName + ".");
+      message = ". That's not a valid entry, " + userName + ".";
     }
   }
+  document.getElementById("question4").innerHTML = answer + message;
 }
 numberGuess();
 
-alert("Great job " + userName + " - you got " + counter + " questions right!");
+document.getElementById("question5").innerHTML = "Great job " + userName + " - you got " + counter + " questions right!";
